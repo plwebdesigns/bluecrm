fb2ab644af96c3f8ef06c9cad69c99922db7649f
Paul Longo <paullongo@outlook.com>
2021-04-25 23:27:43 +0000

    updated migrations

--- database/migrations/2019_10_13_121556_create_users_table.php
+++ database/migrations/2019_10_13_121556_create_users_table.php
@@ -17,16 +17,21 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('agent_name');
+            $table->string('title', 50);
             $table->string('phone', 12)->nullable(true);
             $table->string('email')->unique()->nullable(true);
+            $table->string('dob')->default('N/A');
+            $table->float('current_split')->default(0.95);
+            $table->float('membership_fee')->default(299.00);
             $table->string('password')->default('password123');
             $table->string('api_token', 80)->unique()->nullable(true);
             $table->boolean('isAdmin')->nullable(true)->default(false);
             $table->boolean('isManager')->nullable(true)->default(false);
             $table->string('picture_url')->nullable(true);
-            $table->integer('membership')->default(0);
+            $table->integer('membership_dues_paid')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->string('added_by');
         });
     }
 
